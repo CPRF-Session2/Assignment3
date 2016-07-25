7656cb9ccbb6a55f2c87a4b2d852a5d17fcb4b64
madeleine4223 <mc4223@columbia.edu>
2016-07-25 15:54:40 -0400

    counts c

--- Ccount.c
+++ Ccount.c
@@ -1,33 +1,14 @@
 /*Write a program Ccount.c that reads in a string of user-input characters and counts the number of times C (either uppercase or lowercase) appears. For instance, if the input is “sdfxCkkwixcpcwacdjc”, the letter C appears 5 times. */
 #include <stdio.h>
 int main() {
-	int num=0;
-	int i;
 	printf("Enter a string:\n");
 	char word;
-	for(i=0;i<1;i++)
-	{	
-		scanf("%c", &word);
-	}
-	for (i=0;i<1;i++)
-		printf("%c\n", word);
-	for(i=0;i<1;i++)
-	{
-	switch (word) {
-		case ('c'):
-		case('C'):
-			num++;
-			continue;
-		default:
-			continue;
-	}
-	/*	while (1)
-	{
-		if (string== 'C'||'c')
-			num++;
-	}
-	printf("%c has %d C's", string,num);
-*/	
+	int counter= 0;
+	do {
+		scanf("%c",&word);
+		if (word=='c'||word=='C')
+		{		counter++;}
+	} while (word!='\n');
+	printf("Your string has %d \"c\"\'s\n", counter);
 	return 0;
-	}
 	}
